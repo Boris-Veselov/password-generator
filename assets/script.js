9bd01e955d25008e8cc93fd039f75907a16582a0
Boris Veselov <borisveselov@yahoo.com>
2022-02-17 22:29:18 -0500

    Arrays & functions

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,41 @@
-// Get references to the #generate element
+var characterLength = 8;
+var choiceArr = [];
+
+//Arrays
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')'];
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCassArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numericArr = ['1','2','3','4','5','6','7','8','9','0',];
 
 var generateBtn = document.querySelector("#generate");
-
-function generatePassword() {
-  
-  
-
   // 1 prompt the user for password criteria
   // pass length 8 < 128
   // lowercase, uppercase, special characters
-  var length = Number(prompt ("How long?"))
-  console.log(length)
-  
-  var lowercase = confirm("Like lowercase?")
-  console.log(lowercase)
-  var uppercase = confirm("like uppercase?")
-  console.log(uppercase)
-  var numeric = confirm("Like numeric characters?")
-  console.log(numeric)
-  var specialCharacters = confirm("Like special characters")
-  console.log(specialCharacters)
-
   // 2 validate the input
-
-if (lowercase + uppercase + numeric + specialCharacters <= 0)
-  return;
-
- 
- 
   // 3 generate password based on criteria
-
-  document.getElementById("password").value = password;
-      
-   
   // 4 display password to the page
-  return "";
-}
-
-
-// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if(correctPrompts) {
+    var password = generatePassword();
+    
 
+    passwordText.value = password;
+  } else {
+    passwordText.value = "";
+}
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomletter = Math.floor(Math.random() * choiceArr.length)
+    password = password + choiceArr[randomletter];
+  }
+  return password;
+}
+
+
+generateBtn.addEventListener('click', writePassword);
