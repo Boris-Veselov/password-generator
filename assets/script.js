d3243c071f503b3ca4a4858d85f1601b7e11e8c7
Boris Veselov <borisveselov@yahoo.com>
2022-02-17 20:47:06 -0500

    password criteria

--- assets/script.js
+++ assets/script.js
@@ -9,18 +9,32 @@ function generatePassword() {
   // 1 prompt the user for password criteria
   // pass length 8 < 128
   // lowercase, uppercase, special characters
- 
+  var length = Number(prompt ("How long?"))
+  console.log(length)
+  
+  var lowercase = confirm("Like lowercase?")
+  console.log(lowercase)
+  var uppercase = confirm("like uppercase?")
+  console.log(uppercase)
+  var numeric = confirm("Like numeric characters?")
+  console.log(numeric)
+  var specialCharacters = confirm("Like special characters")
+  console.log(specialCharacters)
 
   // 2 validate the input
-  
+
+if (lowercase + uppercase + numeric + specialCharacters <= 0)
+  return;
+
+ 
  
   // 3 generate password based on criteria
-  
+
+  document.getElementById("password").value = password;
       
    
   // 4 display password to the page
-  
-  return "Generated password goes here";
+  return "";
 }
 
 
@@ -35,6 +49,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
